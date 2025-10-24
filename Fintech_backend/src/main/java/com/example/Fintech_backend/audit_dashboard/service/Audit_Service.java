e64bae112817862a6279b9e2d6f990a7d75d7634
Faraaz304 <faraazshaikh304@gmail.com>
2025-10-24 16:38:29 +0530

    made audit sevice interface

---
+++ Fintech_backend/src/main/java/com/example/Fintech_backend/audit_dashboard/service/Audit_Service.java
@@ -0,0 +1,20 @@
+package com.example.Fintech_backend.audit_dashboard.service;
+
+import java.lang.foreign.Linker.Option;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import com.example.Fintech_backend.account.dto.AccountDto;
+import com.example.Fintech_backend.auth_user.dto.UserDto;
+import com.example.Fintech_backend.transaction.dto.TransactionDto;
+
+public interface Audit_Service {
+    Map<String, Long> getSystemTotals();
+    Optional<UserDto> findUserbyEmail(String email);
+    Optional<AccountDto> findAccountDetailsByAccountNumber(String accountNumber);
+    List<TransactionDto> findTransactionByAccountNumber(String accountNumber);
+    Optional<TransactionDto> findTransactionById(Long transactionId);
+      
+    
+} 
